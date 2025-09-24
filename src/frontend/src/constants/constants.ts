c5375a2065e0c1dfe83abcaecd6d2e572ad4a2c4
deepmindru-afk <deepmindru@gmail.com>
2025-09-24 23:48:42 +0300

    Update constants.ts

--- src/frontend/src/constants/constants.ts
+++ src/frontend/src/constants/constants.ts
@@ -145,7 +145,7 @@ export const EDIT_DIALOG_SUBTITLE =
  * @constant
  */
 export const CODE_PROMPT_DIALOG_SUBTITLE =
-  "Edit your Python code snippet. Refer to the Langflow documentation for more information on how to write your own component.";
+  "Edit your Python code snippet. Refer to the Portflow documentation for more information on how to write your own component.";
 
 export const CODE_DICT_DIALOG_SUBTITLE =
   "Customize your dictionary, adding or editing key-value pairs as needed. Supports adding new objects {} or arrays [].";
@@ -639,7 +639,7 @@ export const TIMEOUT_ERROR_DESCRIPION = "Server is busy.";
 export const SIGN_UP_SUCCESS = "Account created! Await admin activation. ";
 
 export const API_PAGE_PARAGRAPH =
-  "Your secret Langflow API keys are listed below. Do not share your API key with others, or expose it in the browser or other client-side code.";
+  "Your secret Portflow API keys are listed below. Do not share your API key with others, or expose it in the browser or other client-side code.";
 
 export const API_PAGE_USER_KEYS =
   "This user does not have any keys assigned at the moment.";
@@ -720,7 +720,7 @@ export const INPUT_HANDLER_HOVER = "Avaliable input components:";
 export const OUTPUT_HANDLER_HOVER = "Avaliable output components:";
 export const TEXT_INPUT_MODAL_TITLE = "Inputs";
 export const OUTPUTS_MODAL_TITLE = "Outputs";
-export const LANGFLOW_CHAT_TITLE = "Langflow Chat";
+export const LANGFLOW_CHAT_TITLE = "Portflow Chat";
 export const CHAT_INPUT_PLACEHOLDER =
   "No chat input variables found. Click to run your flow.";
 export const CHAT_INPUT_PLACEHOLDER_SEND = "Send a message...";
@@ -728,7 +728,7 @@ export const EDIT_CODE_TITLE = "Edit Code";
 export const MY_COLLECTION_DESC =
   "Manage your projects. Download and upload entire collections.";
 export const STORE_DESC = "Explore community-shared flows and components.";
-export const STORE_TITLE = "Langflow Store";
+export const STORE_TITLE = "Portflow Store";
 export const NO_API_KEY = "You don't have an API key.";
 export const INSERT_API_KEY = "Insert your Langflow API key.";
 export const INVALID_API_KEY = "Your API key is not valid. ";
@@ -1098,13 +1098,13 @@ export const OPENAI_VOICES = [
 export const DEFAULT_POLLING_INTERVAL = 5000;
 export const DEFAULT_TIMEOUT = 30000;
 export const DEFAULT_FILE_PICKER_TIMEOUT = 60000;
-export const DISCORD_URL = "https://discord.com/invite/EqksyE2EX9";
-export const GITHUB_URL = "https://github.com/langflow-ai/langflow";
-export const TWITTER_URL = "https://x.com/langflow_ai";
-export const DOCS_URL = "https://docs.langflow.org";
-export const DATASTAX_DOCS_URL =
-  "https://docs.datastax.com/en/langflow/index.html";
-export const DESKTOP_URL = "https://www.langflow.org/desktop";
-export const BUG_REPORT_URL = "https://github.com/langflow-ai/langflow/issues";
+export const DISCORD_URL =  "https://www.portalos.ru" //"https://discord.com/invite/EqksyE2EX9";
+export const GITHUB_URL =  "https://www.portalos.ru" //"https://github.com/langflow-ai/langflow";
+export const TWITTER_URL =  "https://www.portalos.ru"//"https://x.com/langflow_ai";
+export const DOCS_URL =  "https://www.portalos.ru"//"https://docs.langflow.org";
+export const DATASTAX_DOCS_URL =  "https://www.portalos.ru"
+  //"https://docs.datastax.com/en/langflow/index.html";
+export const DESKTOP_URL = "https://www.portalos.ru" //"https://www.langflow.org/desktop";
+export const BUG_REPORT_URL = "https://www.portalos.ru" //"https://github.com/langflow-ai/langflow/issues";
 
 export const UUID_PARSING_ERROR = "uuid_parsing";
