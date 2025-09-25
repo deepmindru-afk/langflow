5b4410cd8ef5740dfa300a1e0b0c4ef0263cc0d4
smatry <smartouses@gmail.com>
2025-09-25 19:09:52 +0300

    cmt

--- src/frontend/src/customization/feature-flags.ts
+++ src/frontend/src/customization/feature-flags.ts
@@ -2,11 +2,11 @@ export const ENABLE_DARK_MODE = true;
 export const ENABLE_API = true;
 export const ENABLE_LANGFLOW_STORE = false;
 export const ENABLE_PROFILE_ICONS = true;
-export const ENABLE_SOCIAL_LINKS = true;
+export const ENABLE_SOCIAL_LINKS = false;
 export const ENABLE_BRANDING = true;
 export const ENABLE_MVPS = false;
-export const ENABLE_CUSTOM_PARAM = false;
-export const ENABLE_INTEGRATIONS = false;
+export const ENABLE_CUSTOM_PARAM = true;
+export const ENABLE_INTEGRATIONS = true;
 export const ENABLE_DATASTAX_LANGFLOW = false;
 export const ENABLE_FILE_MANAGEMENT = true;
 export const ENABLE_PUBLISH = true;
@@ -15,7 +15,7 @@ export const ENABLE_VOICE_ASSISTANT = true;
 export const ENABLE_IMAGE_ON_PLAYGROUND = false;
 export const ENABLE_MCP = true;
 export const ENABLE_MCP_NOTICE = false;
-export const ENABLE_KNOWLEDGE_BASES = false;
+export const ENABLE_KNOWLEDGE_BASES = true;
 
 export const ENABLE_MCP_COMPOSER =
   process.env.LANGFLOW_MCP_COMPOSER_ENABLED === "true";
